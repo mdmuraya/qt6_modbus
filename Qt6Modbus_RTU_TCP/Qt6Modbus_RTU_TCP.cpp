de7771da5b8948c7b1cada251aa7f75d322fce8c
mdmuraya <mdmuraya@gmail.com>
2026-01-03 04:33:22 -0500

    Made somne chnges

--- Qt6Modbus_RTU_TCP/Qt6Modbus_RTU_TCP.cpp
+++ Qt6Modbus_RTU_TCP/Qt6Modbus_RTU_TCP.cpp
@@ -23,13 +23,14 @@ void Qt6Modbus_RTU_TCP::onConnectToVFD(QString port)
     if (modbusDevice->state() != QModbusDevice::ConnectedState)
     {
         modbusDevice->setConnectionParameter(QModbusDevice::SerialPortNameParameter, port);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialParityParameter,QSerialPort::Parity::EvenParity);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialBaudRateParameter,QSerialPort::BaudRate::Baud9600);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialDataBitsParameter,QSerialPort::DataBits::Data8);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialStopBitsParameter,QSerialPort::StopBits::OneStop);
+        modbusDevice->setConnectionParameter(QModbusDevice::SerialParityParameter,QSerialPort::EvenParity);
+        modbusDevice->setConnectionParameter(QModbusDevice::SerialBaudRateParameter,QSerialPort::Baud9600);
+        modbusDevice->setConnectionParameter(QModbusDevice::SerialDataBitsParameter,QSerialPort::Data8);
+        modbusDevice->setConnectionParameter(QModbusDevice::SerialStopBitsParameter,QSerialPort::OneStop);
 
         modbusDevice->setTimeout(10000);
         modbusDevice->setNumberOfRetries(3);
+
 
         if (!modbusDevice->connectDevice()) {
             //statusBar()->showMessage(tr("Connect failed: %1").arg(modbusDevice->errorString()), 5000);
@@ -38,6 +39,49 @@ void Qt6Modbus_RTU_TCP::onConnectToVFD(QString port)
             //ui->actionConnect->setEnabled(false);
             //ui->actionDisconnect->setEnabled(true);
             qDebug() << "Qt6Modbus_RTU_TCP::onConnectToVFD() on COM port: " + port + " SUCCESS";
+
+            if (modbusDevice->state() == QModbusDevice::ConnectedState)
+            {
+                QModbusDataUnit readHoldingRegisters(QModbusDataUnit::HoldingRegisters, 0, 10);
+                int modbusDeviceId = 45;
+
+                //QModbusDataUnit ReadUnit(QModbusDataUnit::HoldingRegisters, 0, 10);
+
+                if (auto *reply = modbusDevice->sendReadRequest(readHoldingRegisters, modbusDeviceId))
+                {
+                    if (!reply->isFinished())
+                    {
+                        connect(reply, &QModbusReply::finished, this, [this, reply]()
+                            {
+                                auto reply_1 =  qobject_cast<QModbusReply*>(sender());
+                                if (!reply_1) return;
+
+                                if (reply_1->error() == QModbusDevice::NoError)
+                                {
+                                    qDebug() << "Data read sucessfully";
+                                    //statusBar()->showMessage("Data read sucessfully");
+
+                                    const QModbusDataUnit units = reply_1->result();
+                                    //ui->listWidget_Holding_Data->clear();
+                                    for (int i = 0 ; i < units.valueCount() ; i++)
+                                    {
+                                        QString entry = "Address : " + QString::number(units.startAddress() + i) + " Values : " + QString::number(units.value(i), 16) + " | " + QString::number(units.value(i));
+                                        //ui->listWidget_Holding_Data->addItem(Entry);
+                                        qDebug() << "Data read sucessfully: " + entry;
+                                    }
+                                }
+                                else
+                                {
+                                    qDebug() << "Data read failed";
+                                   // statusBar()->showMessage("Data read failed : " + reply_1->errorString());
+                                }
+                        });
+                    }
+                }
+
+
+            }
+
         }
 
     }
@@ -46,21 +90,165 @@ void Qt6Modbus_RTU_TCP::onConnectToVFD(QString port)
 void Qt6Modbus_RTU_TCP::onClearVFDFaults()
 {
     qDebug() << "Qt6Modbus_RTU_TCP::onClearVFDFaults()";
+
+    if (modbusDevice->state() == QModbusDevice::ConnectedState)
+    {
+        int modbusDeviceId = 45;
+        int addressToWrite = 8192;
+
+        QVector<quint16> dataToWrite;
+        dataToWrite.append(8); // Clear VFD Faults
+
+        QModbusDataUnit writeHoldingRegisters(QModbusDataUnit::HoldingRegisters, addressToWrite, dataToWrite.size());
+
+        for (int i = 0 ; i < dataToWrite.size() ; i++)
+        {
+            writeHoldingRegisters.setValue(i, dataToWrite.at(i));
+        }
+
+        if (auto *reply = modbusDevice->sendWriteRequest(writeHoldingRegisters, modbusDeviceId))
+        {
+            if (!reply->isFinished())
+            {
+                connect(reply, &QModbusReply::finished, this, [this, reply]()
+                {
+                    if (reply->error() == QModbusDevice::NoError)
+                    {
+                        qDebug() << "Data is write sucessfully";
+                        //statusBar()->showMessage("Data is write sucessfully");
+                    }
+                    else
+                    {
+                        qDebug() << "Data is write failed";
+                        //statusBar()->showMessage("Data is write failed : " + reply->errorString());
+                    }
+                });
+            }
+        }
+    }
 }
 
 void Qt6Modbus_RTU_TCP::onStartMotorFWD()
 {
     qDebug() << "Qt6Modbus_RTU_TCP::onStartMotorFWD()";
+
+    if (modbusDevice->state() == QModbusDevice::ConnectedState)
+    {
+        int modbusDeviceId = 45;
+        int addressToWrite = 8192;
+
+        QVector<quint16> dataToWrite;
+        dataToWrite.append(18); // Start Motor FWD
+
+        QModbusDataUnit writeHoldingRegisters(QModbusDataUnit::HoldingRegisters, addressToWrite, dataToWrite.size());
+
+        for (int i = 0 ; i < dataToWrite.size() ; i++)
+        {
+            writeHoldingRegisters.setValue(i, dataToWrite.at(i));
+        }
+
+        if (auto *reply = modbusDevice->sendWriteRequest(writeHoldingRegisters, modbusDeviceId))
+        {
+            if (!reply->isFinished())
+            {
+                connect(reply, &QModbusReply::finished, this, [this, reply]()
+                        {
+                            if (reply->error() == QModbusDevice::NoError)
+                            {
+                                qDebug() << "Data is write sucessfully";
+                                //statusBar()->showMessage("Data is write sucessfully");
+                            }
+                            else
+                            {
+                                qDebug() << "Data is write failed";
+                                //statusBar()->showMessage("Data is write failed : " + reply->errorString());
+                            }
+                        });
+            }
+        }
+    }
 }
 
 void Qt6Modbus_RTU_TCP::onStartMotorREV()
 {
     qDebug() << "Qt6Modbus_RTU_TCP::onStartMotorREV()";
+
+    if (modbusDevice->state() == QModbusDevice::ConnectedState)
+    {
+        int modbusDeviceId = 45;
+        int addressToWrite = 8192;
+
+        QVector<quint16> dataToWrite;
+        dataToWrite.append(34); // Start Motor REV
+
+        QModbusDataUnit writeHoldingRegisters(QModbusDataUnit::HoldingRegisters, addressToWrite, dataToWrite.size());
+
+        for (int i = 0 ; i < dataToWrite.size() ; i++)
+        {
+            writeHoldingRegisters.setValue(i, dataToWrite.at(i));
+        }
+
+        if (auto *reply = modbusDevice->sendWriteRequest(writeHoldingRegisters, modbusDeviceId))
+        {
+            if (!reply->isFinished())
+            {
+                connect(reply, &QModbusReply::finished, this, [this, reply]()
+                        {
+                            if (reply->error() == QModbusDevice::NoError)
+                            {
+                                qDebug() << "Data is write sucessfully";
+                                //statusBar()->showMessage("Data is write sucessfully");
+                            }
+                            else
+                            {
+                                qDebug() << "Data is write failed";
+                                //statusBar()->showMessage("Data is write failed : " + reply->errorString());
+                            }
+                        });
+            }
+        }
+    }
 }
 
 void Qt6Modbus_RTU_TCP::onStopMotor()
 {
     qDebug() << "Qt6Modbus_RTU_TCP::onStopMotor()";
+
+    if (modbusDevice->state() == QModbusDevice::ConnectedState)
+    {
+        int modbusDeviceId = 45;
+        int addressToWrite = 8192;
+
+        QVector<quint16> dataToWrite;
+        dataToWrite.append(1); // Staop Motor
+
+        QModbusDataUnit writeHoldingRegisters(QModbusDataUnit::HoldingRegisters, addressToWrite, dataToWrite.size());
+
+        for (int i = 0 ; i < dataToWrite.size() ; i++)
+        {
+            writeHoldingRegisters.setValue(i, dataToWrite.at(i));
+        }
+
+        if (auto *reply = modbusDevice->sendWriteRequest(writeHoldingRegisters, modbusDeviceId))
+        {
+            if (!reply->isFinished())
+            {
+                connect(reply, &QModbusReply::finished, this, [this, reply]()
+                        {
+                            if (reply->error() == QModbusDevice::NoError)
+                            {
+                                qDebug() << "Data is write sucessfully";
+                                //statusBar()->showMessage("Data is write sucessfully");
+                            }
+                            else
+                            {
+                                qDebug() << "Data is write failed";
+                                //statusBar()->showMessage("Data is write failed : " + reply->errorString());
+                            }
+                        });
+            }
+        }
+    }
 }
 
 
